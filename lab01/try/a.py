825fe77bd9af9d6f6a8eea8d5e62c12bf6a655ad
huiyang.wang621@gmail.com <whyshh415621>
2020-02-22 01:24:33 +1300

    add img show

--- lab01/try/a.py
+++ lab01/try/a.py
@@ -5,6 +5,21 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread('a.jpg', cv2.IMREAD_GRAYSCALE)
 
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyWindow()
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyWindow()
+
+plt.imshow(img, cmap='gray', interpolation='bicubic')
+plt.show()
+
+
+cap = cv2.VideoCapture('./a.webm')
+while True:
+    ret, frame = cap.read()
+    # plt.imshow('frame', frame)
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
